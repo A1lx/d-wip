8add2a9223bda9b133943269f49be0b9555dc2e9
Aleksey Klimov <alx111wrk@gmail.com>
2024-01-24 14:13:50 +0300

    3 ver

--- src/components/MainPage/SearchTickets/SearchTickets.tsx
+++ src/components/MainPage/SearchTickets/SearchTickets.tsx
@@ -1,7 +1,18 @@
 import "./searchTickets.css";
 import swapIcon from "./img/swap-icon.svg";
+import { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import ru from 'date-fns/locale/ru';
+import { registerLocale, setDefaultLocale } from "react-datepicker";
+
+registerLocale('ru', ru);
+setDefaultLocale('ru');
 
 export const SearchTickets = () => {
+  const [startDate, setStartDate] = useState();
+  const [endDate, setEndDate] = useState();
+
   return (
     <div className="search-tickets">
       <form className="search-tickets__form" autoComplete="off">
@@ -32,20 +43,30 @@ export const SearchTickets = () => {
         <span >Дата</span>
         <div className="search-tickets__form-block search-tickets__form-block_date">
           <div>
-            <label htmlFor="search-tickets-form"></label>
+            <DatePicker 
+              className="search-tickets__form-input" 
+              selected={startDate}
+              placeholderText="ДД/ММ/ГГ"
+              onChange={(date) => setStartDate(date)}/>
+            {/* <label htmlFor="search-tickets-form"></label>
             <input 
               id="search-tickets-form" 
               name="search-tickets-form" 
               className="search-tickets__form-input" 
-              type="date"/>
+              type="date"/> */}
           </div>
           <div>
-            <label htmlFor="search-tickets-form"></label>
+            <DatePicker 
+              className="search-tickets__form-input" 
+              selected={endDate}
+              placeholderText="ДД/ММ/ГГ"
+              onChange={(date) => setEndDate(date)}/>
+            {/* <label htmlFor="search-tickets-form"></label>
             <input 
               id="search-tickets-form" 
               name="search-tickets-form" 
               className="search-tickets__form-input" 
-              type="date"/>
+              type="date"/> */}
           </div>
         </div>
         <div className="search-tickets__form-btn-box">
